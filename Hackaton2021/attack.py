66f2c9f1e3b44fea8d2436bcb41b01d9af1b986c
RobeDM <rober.diaz@gmail.com>
2021-09-21 14:09:26 +0200

    Commented attack.py

--- Hackaton2021/attack.py
+++ Hackaton2021/attack.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 '''
-Available optimization strategies for gradient averaging for Neural Networks in Master node.
+This file contains the abstract class to implement an attack and also two instances that must be implemented by the participants:
+
+- ImplementedAttack1
+
+- ImplementedAttack2
+
+This attack will be used by two workers in the hackathon, the goal is to reduce the accuracy in a scenario where 8 workers are honest and 2 workers are malicious.
+
 '''
 
 __author__ = "Roberto Díaz Morales"
@@ -15,21 +22,44 @@ from abc import ABCMeta, abstractmethod
 
 class Attack(object, metaclass=ABCMeta):
     """
-    This class implements the different gradient optimizers, run at Master node.
+    This class conains two methods, preprocess and process that can be used to implement an attack, they run in a worker node.
     """
 
     @abstractmethod
     def preprocess(self, Xtr_b, ytr):
         """
-        Aggregate the gradients received from a set of workers.
+        This function is called once, before the training process starts.
+
+        It can be used to make changes on the local dataset that will remain along the training process and also to compute statistics based on the local training set
+        instead of perform these computations in every iteration of the training process. It can be also used to create atributes for the object instantiated with this class.
 
         Parameters
         ----------
-        model: :class:`NN_model`
-            Neural Network model object.
+        Xtr_b: numpy array
+            A numpy matrix where every row is a sample and every column a feature.
 
-        list_gradients: list
-            List containing the gradients of the different workers.
+        ytr: numpy array
+            The class of every data sample in a one hot encoding format:
+
+            [[1 0 0 0]    -> The first sample belongs to class 1
+            [0 0 1 0]     -> The first sample belongs to class 3
+            [0 1 0 0]     -> The first sample belongs to class 2
+            [0 1 0 0]]    -> The first sample belongs to class 2
+ 
+
+        Returns
+        ----------
+        Xtr_final: numpy array
+            A numpy matrix where every row is a sample and every column a feature. This matrix will be used in the training process
+
+        ytr_final: numpy array
+            The label of every data sample in a one hot encoding format, this labels will be used in the training process:
+
+            [[1 0 0 0]    -> The first sample belongs to class 1
+            [0 0 1 0]     -> The first sample belongs to class 3
+            [0 1 0 0]     -> The first sample belongs to class 2
+            [0 1 0 0]]    -> The first sample belongs to class 2
+
         """
         pass
 
@@ -38,15 +68,29 @@ class Attack(object, metaclass=ABCMeta):
     def process(self, model, weights, Xtr_b, ytr, epochs, batch_size):
 
         """
-        Aggregate the gradients received from a set of workers.
+        This function receives a neural network, the training set and the number of epochs and batch size requested by the master node. It trains the neural network. 
 
         Parameters
         ----------
-        model: :class:`NN_model`
-            Neural Network model object.
+        model: Keras model
+            The neural network used in the local training process
 
-        list_gradients: list
-            List containing the gradients of the different workers.
+        weights: list of numpy arrays
+            The weights received by the master node that will be used to initializate the neural network.
+
+        Xtr_b: numpy array
+            The local training set. A numpy matrix where every row is a sample and every column a feature.
+
+        ytr: numpy array
+            The labels of the local training set. The class of every data sample in a one hot encoding format.
+
+        epochs: int
+            Number of training epochs requested by the master node.
+
+        batch_size: int
+            The batch size requested by the master node to be used during training.
+
+
         """
         pass
 
@@ -59,40 +103,88 @@ class ImplementedAttack1(Attack):
 
     def __init__(self):
         """
-        Create a :class:`ModelAveraging` instance.
+        Create a :class:`ImplementedAttack1` instance.
 
         """
 
 
     def preprocess(self, Xtr_b, ytr):
         """
-        Aggregate the gradients received from a set of workers.
+        This function is called once, before the training process starts.
+
+        It can be used to make changes on the local dataset that will remain along the training process and also to compute statistics based on the local training set
+        instead of perform these computations in every iteration of the training process. It can be also used to create atributes for the object instantiated with this class.
 
         Parameters
         ----------
-        model: :class:`NN_model`
-            Neural Network model object.
+        Xtr_b: numpy array
+            A numpy matrix where every row is a sample and every column a feature.
 
-        list_gradients: list
-            List containing the gradients of the different workers.
+        ytr: numpy array
+            The class of every data sample in a one hot encoding format:
+
+            [[1 0 0 0]    -> The first sample belongs to class 1
+            [0 0 1 0]     -> The first sample belongs to class 3
+            [0 1 0 0]     -> The first sample belongs to class 2
+            [0 1 0 0]]    -> The first sample belongs to class 2
+ 
+
+        Returns
+        ----------
+        Xtr_final: numpy array
+            A numpy matrix where every row is a sample and every column a feature. This matrix will be used in the training process
+
+        ytr_final: numpy array
+            The label of every data sample in a one hot encoding format, this labels will be used in the training process:
+
+            [[1 0 0 0]    -> The first sample belongs to class 1
+            [0 0 1 0]     -> The first sample belongs to class 3
+            [0 1 0 0]     -> The first sample belongs to class 2
+            [0 1 0 0]]    -> The first sample belongs to class 2
+
         """
-        return Xtr_b,ytr
+
+        ########################################
+        # TO BE MODIFIED BY THE PARTICIPANTS
+        Xtr_final = Xtr_b.clone()  # This is an example with no attack, the dataset is not modified
+        ytr_final = ytr.clone()    # This is an example with no attack, the labels are not modified
+        #########################################
+
+        return Xtr_final,ytr_final
 
     def process(self, model, weights, Xtr_b, ytr, epochs, batch_size):
 
         """
-        Aggregate the gradients received from a set of workers.
+        This function receives a neural network, the training set and the number of epochs and batch size requested by the master node. It trains the neural network. 
 
         Parameters
         ----------
-        model: :class:`NN_model`
-            Neural Network model object.
+        model: Keras model
+            The neural network used in the local training process
 
-        list_gradients: list
-            List containing the gradients of the different workers.
+        weights: list of numpy arrays
+            The weights received by the master node that will be used to initializate the neural network.
+
+        Xtr_b: numpy array
+            The local training set. A numpy matrix where every row is a sample and every column a feature.
+
+        ytr: numpy array
+            The labels of the local training set. The class of every data sample in a one hot encoding format.
+
+        epochs: int
+            Number of training epochs requested by the master node.
+
+        batch_size: int
+            The batch size requested by the master node to be used during training.
+
+
         """
-        model.keras_model.set_weights(weights)
-        model.keras_model.fit(Xtr_b, ytr, epochs=epochs, batch_size=batch_size, verbose=1)
+
+        ########################################
+        # TO BE MODIFIED BY THE PARTICIPANTS
+        model.keras_model.set_weights(weights)                                                # This is an example with no attack, first we load the weights in the neural network.
+        model.keras_model.fit(Xtr_b, ytr, epochs=epochs, batch_size=batch_size, verbose=1)    # This is an example with no attack, we just train the neural network.
+        #########################################
 
 
 class ImplementedAttack2(Attack):
@@ -103,39 +195,86 @@ class ImplementedAttack2(Attack):
 
     def __init__(self):
         """
-        Create a :class:`ModelAveraging` instance.
+        Create a :class:`ImplementedAttack2` instance.
 
         """
 
 
     def preprocess(self, Xtr_b, ytr):
         """
-        Aggregate the gradients received from a set of workers.
+        This function is called once, before the training process starts.
+
+        It can be used to make changes on the local dataset that will remain along the training process and also to compute statistics based on the local training set
+        instead of perform these computations in every iteration of the training process. It can be also used to create atributes for the object instantiated with this class.
 
         Parameters
         ----------
-        model: :class:`NN_model`
-            Neural Network model object.
+        Xtr_b: numpy array
+            A numpy matrix where every row is a sample and every column a feature.
 
-        list_gradients: list
-            List containing the gradients of the different workers.
+        ytr: numpy array
+            The class of every data sample in a one hot encoding format:
+
+            [[1 0 0 0]    -> The first sample belongs to class 1
+            [0 0 1 0]     -> The first sample belongs to class 3
+            [0 1 0 0]     -> The first sample belongs to class 2
+            [0 1 0 0]]    -> The first sample belongs to class 2
+ 
+
+        Returns
+        ----------
+        Xtr_final: numpy array
+            A numpy matrix where every row is a sample and every column a feature. This matrix will be used in the training process
+
+        ytr_final: numpy array
+            The label of every data sample in a one hot encoding format, this labels will be used in the training process:
+
+            [[1 0 0 0]    -> The first sample belongs to class 1
+            [0 0 1 0]     -> The first sample belongs to class 3
+            [0 1 0 0]     -> The first sample belongs to class 2
+            [0 1 0 0]]    -> The first sample belongs to class 2
+
         """
-        return Xtr_b,ytr
+        ########################################
+        # TO BE MODIFIED BY THE PARTICIPANTS
+        Xtr_final = Xtr_b.clone()  # This is an example with no attack, the dataset is not modified
+        ytr_final = ytr.clone()    # This is an example with no attack, the labels are not modified
+        #########################################
+
+        return Xtr_final,ytr_final
+
 
     def process(self, model, weights, Xtr_b, ytr, epochs, batch_size):
 
         """
-        Aggregate the gradients received from a set of workers.
+        This function receives a neural network, the training set and the number of epochs and batch size requested by the master node. It trains the neural network. 
 
         Parameters
         ----------
-        model: :class:`NN_model`
-            Neural Network model object.
+        model: Keras model
+            The neural network used in the local training process
 
-        list_gradients: list
-            List containing the gradients of the different workers.
+        weights: list of numpy arrays
+            The weights received by the master node that will be used to initializate the neural network.
+
+        Xtr_b: numpy array
+            The local training set. A numpy matrix where every row is a sample and every column a feature.
+
+        ytr: numpy array
+            The labels of the local training set. The class of every data sample in a one hot encoding format.
+
+        epochs: int
+            Number of training epochs requested by the master node.
+
+        batch_size: int
+            The batch size requested by the master node to be used during training.
+
+
         """
-        model.keras_model.set_weights(weights)
-        model.keras_model.fit(Xtr_b, ytr, epochs=epochs, batch_size=batch_size, verbose=1)
+        ########################################
+        # TO BE MODIFIED BY THE PARTICIPANTS
+        model.keras_model.set_weights(weights)                                                # This is an example with no attack, first we load the weights in the neural network.
+        model.keras_model.fit(Xtr_b, ytr, epochs=epochs, batch_size=batch_size, verbose=1)    # This is an example with no attack, we just train the neural network.
+        #########################################
 
 
